39656558d7763f49b29bc36832b7d4344879ab9c
Anna Sedlar <asedlar@bignerdranch.com>
2018-07-23 20:48:59 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var fs = require('fs');
+var extract = require('./extract');
+var wss = require('./websockets-server');
+
+var handleError = function(err, res) {
+  res.writeHead(404);
+  res.end();
+}
+
+var server = http.createServer(function (req, res) {
+  console.log('Responding to a request.');
+  var filePath = extract(req.url);
+  fs.readFile(filePath, function(err, data){
+    if (err) {
+      handleError(err, res);
+      return;
+    } else {
+      console.log("Testing");
+      res.setHeader('Content-Type', 'text/html');
+      res.end(data);
+    }
+  })
+});
+
+server.listen(3000);
